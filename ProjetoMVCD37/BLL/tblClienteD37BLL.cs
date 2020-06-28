83b75e4e79af641aacf17e0b6ae017f94f180925
vinicius-santos-programmer <vinicius.a.moreira.santos@gmail.com>
2020-06-28 17:16:46 -0300

    Código da camada BLL que contém as regras de negócio.

---
+++ ProjetoMVCD37/BLL/tblClienteD37BLL.cs
@@ -0,0 +1,29 @@
+﻿using ProjetoMVCD37.DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoMVCD37.BLL
+{
+    public class tblClienteD37BLL
+    {
+        private DALMVC dalMVC = new DALMVC();
+
+        public Boolean AutenticarCampos(int cpf, string nomeCompleto, string nomeMae)
+        {
+            string query = string.Format($@"select * from tbl_clienteD37 where nome_cliente = '{nomeCompleto}' and nome_mae = '{nomeMae}' and cpf_cliente = '{cpf}';");
+            DataTable dt = dalMVC.executarComandoDQL(query);
+            if (dt.Rows.Count == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ProjetoMVCD37/DTO/tblClienteD37DTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoMVCD37.DTO
+{
+    public class TblClienteD37DTO
+    {
+        private int id_cliente, tp_usuario;
+        private string nome_cliente, sobrenome_cliente, email_cliente, senha_cliente, cpf_cliente, nome_mae;
+
+        public int Id_cliente { get => id_cliente; set => id_cliente = value; }
+        public int Tp_usuario { get => tp_usuario; set => tp_usuario = value; }
+        public string Nome_cliente { get => nome_cliente; set => nome_cliente = value; }
+        public string Sobrenome_cliente { get => sobrenome_cliente; set => sobrenome_cliente = value; }
+        public string Email_cliente { get => email_cliente; set => email_cliente = value; }
+        public string Senha_cliente { get => senha_cliente; set => senha_cliente = value; }
+        public string Cpf_cliente { get => cpf_cliente; set => cpf_cliente = value; }
+        public string Nome_mae { get => nome_mae; set => nome_mae = value; }    
+    }
+}
